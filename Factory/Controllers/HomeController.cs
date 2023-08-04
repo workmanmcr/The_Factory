1bfd8766879d3fa262ba8e3824842526a2ec6529
workmanmcr <workmanmcr@gmail.com>
2023-08-04 09:04:18 -0700

    create mvc

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+
+namespace Factory.Controllers;
+
+public class HomeController : Controller
+{
+  
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+   
